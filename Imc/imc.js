bceb6cb4146f95d8035381cece900ffeaeb0c4c8
Tamires Da Silva Bezerra <163886976+tamiressil@users.noreply.github.com>
2025-09-13 22:35:14 -0300

    Add files via upload

---
+++ Imc/imc.js
@@ -0,0 +1,21 @@
+function calcularIMC() {
+    let peso = parseFloat(document.getElementById("peso").value);
+    let altura = parseFloat(document.getElementById("altura").value);
+    let imc = peso / (altura * altura)
+
+    if (imc < 18.5) {
+        mensagem = "Você está abaixo do peso";
+    } else if (imc < 24.9) {
+        mensagem = "Você está dentro do aceitavel";
+    } else if (imc < 29.9) {
+        mensagem = "Você está sobrepeso";
+    } else if (imc < 34.9) {
+        mensagem = "Você está no 1º Grau de obesidade";
+    } else if (imc < 39.9) {
+        mensagem = "Você está no 2º Grau de obesidade";
+    } else if (imc >= 40) {
+        mensagem = "Você está no 3º Grau de obesidade";
+    }
+
+    document.getElementById("resultado").innerText = mensagem;
+}
